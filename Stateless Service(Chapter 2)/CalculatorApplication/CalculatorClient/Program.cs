407f58844a8446d98e080229d96834d664875497
Rafael Pimentel <rafael.pimentel.ef@outlook.com>
2018-03-19 00:20:21 -0400

    Completed second version of calculator app using a client failover to another node

---
+++ Stateless Service(Chapter 2)/CalculatorApplication/CalculatorClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using CalculatorService;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+
+namespace CalculatorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var calculatorClient = ServiceProxy.Create<ICalculatorService>
+                    (new Uri("fabric:/CalculatorApplication/CalculatorService"));
+                var result = calculatorClient.Add(1, 2).Result;
+                Console.WriteLine("Your result is: " + result);
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
---
+++ Stateless Service(Chapter 2)/CalculatorApplication/CalculatorService/ICalculatorService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorService
+{
+    public interface ICalculatorService : IService
+    {
+        Task<string> Add(int a, int b);
+        Task<string> Subtract(int a, int b);
+    }
+}
